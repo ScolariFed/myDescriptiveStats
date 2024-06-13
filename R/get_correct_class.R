b1c152780fe53b7f604c32ff7e197a895a1c145d
Federico Scolari <scolari.fed@gmail.com>
2024-06-13 09:41:27 +0200

    added get_correct_class() function

---
+++ R/get_correct_class.R
@@ -0,0 +1,50 @@
+#obtain useful class indication for every column of a dataframe
+get_correct_class <- function(dataframe,
+                              unq.max.perc=0.15){
+
+  #build dataframe with useful parameters for every column
+  df=data.frame(
+    #name
+    name=colnames(dataframe),
+    #class - tail is used for ordered columns, which return vector of classes
+    class=sapply(dataframe, function(x)tail(class(x),1)),
+    #is all of the data NA?
+    allNA=sapply(dataframe, function(x)all(is.na(x))),
+    #is all of the data an integer?
+    allint=sapply(dataframe, function(x){
+      if(tail(class(x),1)=="numeric"){
+        all(na.omit(floor(x)-x)==0)
+      } else NA }),
+    #% of unique non-NA categories
+    unq_max_perc=sapply(dataframe, function(x){
+      if(tail(class(x),1)!="list" & all(is.na(x))==FALSE){
+        max(table(x))/nrow(dataframe)
+      } else NA })
+  )
+
+  #create new column "tipo_dati" based on the data column characteristics
+  df <- df |>
+    mutate(data_type=case_when(
+      #all NA
+      allNA==TRUE~"allNA",
+      #character, same value in more than 15% of samples (customizable)
+      class=="character" & unq_max_perc>unq.max.perc~"CAT",
+      #character, same value in less than 15% of samples (customizable)
+      class=="character" & unq_max_perc<=unq.max.perc~"discard",
+      #TRUE/FALSE columns or factors are always categorical
+      class%in%c("logical","factor")~"CAT",
+      #double or numeric,no integer
+      (class=="double" | allint==FALSE)~"NUM",
+      #integer, same value in more than 15% of samples (customizable)
+      (class=="integer" | allint==TRUE) & unq_max_perc>unq.max.perc~"CAT",
+      #integer, same value in less than 15% of samples (customizable)
+      (class=="integer" | allint==TRUE) & unq_max_perc<=unq.max.perc~"NUM",
+      #everything else
+      TRUE~"discard"))
+
+  #keep only name of column and new class
+  df = df |>
+    select(name,data_type)
+
+  return(df)
+}
